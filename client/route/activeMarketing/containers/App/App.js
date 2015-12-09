1c102d98207f5a6111dad1847e42dd207e2ad657
marchen <844033231@qq.com>
2015-12-09 15:43:50 +0800

    美化主页

--- client/route/activeMarketing/containers/App/App.js
+++ client/route/activeMarketing/containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { PropTypes, Component } from 'react'
+import style from './App.less'
 import Nav from '../Nav'
 
 export default class App extends Component {
@@ -8,7 +9,7 @@ export default class App extends Component {
 
 	render() {
 		return(
-      <div>
+      <div className="activeMarketing-container">
         <Nav />
         {this.props.children || <div>activeMarketing index</div>}
       </div>
--- client/route/dashboard/containers/App/App.js
+++ client/route/dashboard/containers/App/App.js
@@ -2,6 +2,9 @@ import React, { PropTypes, Component } from 'react'
 import {connect} from 'react-redux'
 import style from './App.less'
 
+
+let timeHander;
+
 class App extends Component {
 
 	static propTypes = {
@@ -12,15 +15,17 @@ class App extends Component {
     this.state = {
       welcome : 'welcome',
     }
+    timeHander = null;
+    window.store.dispatch({type:'TIME'})
   }
 
   componentWillUnmount() {
-    console.log()
+    clearInterval(timeHander)
   }
 
   clickHandle(e) {
-    //setInterval(() => {window.store.dispatch({type:'TIME'})},1000)
-    $TIME()
+    clearInterval(timeHander)
+    timeHander = setInterval(() => {window.store.dispatch({type:'TIME'})},1000)
   }
 
 	render() {
@@ -28,10 +33,10 @@ class App extends Component {
     const {time} = this.props
 
 		return(
-      <div>
+      <div className="dashboard-container">
         <h1>{this.state.welcome}</h1>
         <h2>{time}</h2>
-        <div className='start-time' onClick={this.clickHandle.bind(this)}>间</div>
+        <button className='start-time' onClick={this.clickHandle.bind(this)}>点击开始计时</button>
         {this.props.children}
       </div>
 		)
