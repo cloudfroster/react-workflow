c5e62b0716c256df9150ef939236ede8525f2404
marchen <844033231@qq.com>
2015-12-11 00:25:29 +0800

    make webpack config more pretty

--- tasks/webpack.config.js
+++ tasks/webpack.config.js
@@ -1,14 +1,17 @@
+//-------------------------------------------------------------------------
+//  export webpack config
+//-------------------------------------------------------------------------
+
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('lodash.merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+/* const ExtractTextPlugin = require('extract-text-webpack-plugin'); */
 
 const DEBUG = global.DEBUG;
 const VERBOSE = global.VERBOSE;
 const WATCH = global.WATCH;
-const ENTRY = '../client/app.js';  // webpack entry
-const OUTPUT = '../build';  // webpack output
+
 const AUTOPREFIXER = `{
   browsers: [
     'Android >= 4',
@@ -24,19 +27,97 @@ const GLOBALS = {
   'process.env.NODE_ENV': DEBUG ? '"development"' : '"production"',
   __DEV__: DEBUG,
 };
-const JS_LOADER = {
-  test: /\.jsx?$/,
-  exclude: /(node_modules)/,
-  loader: 'babel-loader',
-};
 
-// client-side (app.js)
-// -----------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+//  Configuration for the client-side bundle (app.js)
+//-------------------------------------------------------------------------
 
-const config = {
+const appConfig = {
+
+  entry: [
+    ...(WATCH ? ['webpack-hot-middleware/client'] : []),
+    path.join(__dirname, '../client/app.js'),
+  ],
+
   output: {
+    path: path.join(__dirname, '../build'),
     publicPath: '/',
     sourcePrefix: '',
+    trunkFilename: DEBUG ? '[id].bundle.js' : '[id].[chunkhash].bundle.js',
+    filename: DEBUG ? 'bundle.js' : '[chunkhash].[name].bundle.js',
+  },
+
+  devtool: DEBUG ? 'cheap-module-eval-source-map' : false,
+
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx', '.less'],
+  },
+
+  plugins: [
+
+    /* new ExtractTextPlugin("style.css"), */
+
+    // create index.html
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: 'html?removeOptionalTags=false!./client/index.html',
+      favicon: './client/favicon.ico',
+    }),
+
+    new webpack.optimize.OccurenceOrderPlugin(),
+
+    new webpack.DefinePlugin(GLOBALS),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "commons",
+      filename: DEBUG ? 'commons.js' : '[chunkhash].[name].js',
+      // (Modules must be shared between 2 entries)
+      minChunks: 2,
+      /* chunks: ["pageA", "pageB"], */
+      // (Only use these entries)
+    }),
+
+    ...(!DEBUG ? [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: VERBOSE,
+        },
+      }),
+      //new webpack.optimize.AggressiveMergingPlugin(),
+    ] : []),
+    ...(WATCH ? [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin(),
+    ] : []),
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader',
+      }, {
+        test: /\.txt$/,
+        loader: 'raw-loader',
+      }, {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+        loader: 'url-loader?limit=10000',  // 10kb
+      }, {
+        test: /\.(eot|ttf|wav|mp3)$/,
+        loader: 'file-loader',
+      }, {
+        test: /\.css/,
+        loader: 'style-loader!css-loader?' + (DEBUG ? 'sourceMap' : '') + `!autoprefixer-loader?${AUTOPREFIXER}`,
+      }, {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader?' + (DEBUG ? 'sourceMap' : '') + `!autoprefixer-loader?${AUTOPREFIXER}!less-loader`,
+      },
+    ],
   },
 
   cache: DEBUG,
@@ -54,117 +135,6 @@ const config = {
     cachedAssets: VERBOSE,
   },
 
-  plugins: [
-    new HtmlWebpackPlugin({  // create index.html
-      filename: 'index.html',
-      inject: true,
-      template: 'html?removeOptionalTags=false!./client/index.html',
-      favicon: './client/favicon.ico',
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-  ],
-
-  resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx', '.less'],
-  },
-
-  module: {
-    loaders: [{
-      test: /\.json$/,
-      loader: 'json-loader',
-    }, {
-      test: /\.txt$/,
-      loader: 'raw-loader',
-    }, {
-      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-      loader: 'url-loader?limit=10000',  // 10kb
-    }, {
-      test: /\.(eot|ttf|wav|mp3)$/,
-      loader: 'file-loader',
-    }, ],
-  },
-
 };
-
-//
-// Configuration for the client-side bundle (app.js)
-// -----------------------------------------------------------------------------
-
-const appConfig = merge({}, config, {
-  entry: [
-    ...(WATCH ? ['webpack-hot-middleware/client'] : []),
-    path.join(__dirname, ENTRY),
-  ],
-  output: {
-    trunkFilename: DEBUG ? '[id].bundle.js' : '[id].[chunkhash].bundle.js',
-    path: path.join(__dirname, OUTPUT),
-    filename: DEBUG ? 'bundle.js' : '[chunkhash].[name].bundle.js',
-  },
-
-  // http://webpack.github.io/docs/configuration.html#devtool
-  devtool: DEBUG ? 'cheap-module-eval-source-map' : false,
-  plugins: [
-    ...config.plugins,
-    new webpack.DefinePlugin(GLOBALS),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "commons",
-      // (the commons chunk name)
-
-      filename: DEBUG ? 'commons.js' : '[chunkhash].[name].js',
-      // (the filename of the commons chunk)
-
-      minChunks: 3,
-      // (Modules must be shared between 3 entries)
-
-      // chunks: ["pageA", "pageB"],
-      // (Only use these entries)
-    }),
-    //new ExtractTextPlugin("style.css"),
-    ...(!DEBUG ? [
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: VERBOSE,
-        },
-      }),
-      //new webpack.optimize.AggressiveMergingPlugin(),
-    ] : []),
-    ...(WATCH ? [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin(),
-    ] : []),
-  ],
-  module: {
-    loaders: [
-      WATCH ? merge({}, JS_LOADER
-        /*{  //暂时弃用,等到兼容babel6
-               query: {
-                 // Wraps all React components into arbitrary transforms
-                 // https://github.com/gaearon/babel-plugin-react-transform
-                 plugins: ['react-transform'],
-                 extra: {
-                   'react-transform': {
-                     transforms: [{
-                       transform: 'react-transform-hmr',
-                       imports: ['react'],
-                       locals: ['module'],
-                     }, {
-                       transform: 'react-transform-catch-errors',
-                       imports: ['react', 'redbox-react'],
-                     }, ],
-                   },
-                 },
-               },
-             }*/
-      ) : JS_LOADER,
-      ...config.module.loaders, {
-        test: /\.less$/,
-        loader: 'style-loader!css-loader?' + (DEBUG ? 'sourceMap' : '') + `!autoprefixer-loader?${AUTOPREFIXER}!less-loader`,
-      }, {
-        test: /\.css/,
-        loader: 'style-loader!css-loader?' + (DEBUG ? 'sourceMap' : '') + `!autoprefixer-loader?${AUTOPREFIXER}`,
-      }
-    ],
-  },
-});
 
 module.exports = appConfig;
