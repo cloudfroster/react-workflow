cb32c259319cd9994ddec35628465d3d2f825a62
marchen <844033231@qq.com>
2015-12-11 16:23:33 +0800

    add copy before publish to git

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,9 +1,11 @@
 const ghpages = require('gh-pages');
 const path = require('path');
-
+const fs = require('fs-extra');
 
 function publish() {
   return new Promise((reslove, reject) => {
+
+    copyFileToBuild()
 
     const options = {
       src: '**/*',
@@ -26,6 +28,35 @@ function publish() {
       }
       reslove()
     });
+  })
+}
+
+// copy file to build folder
+function copyFileToBuild() {
+
+  const colors = require('colors')
+
+  function isInArray(value, array) {
+    for(var i = 0, len = array.length; i < len; i++) {
+      if(value === array[i]) {
+        return true
+      }
+    }
+    return false
+  }
+
+  const filter = function(pathname) {
+    const pickFiles = ['README.md', 'processes.json']
+    const name = pathname.split('\\').pop()
+    const dirname = __dirname.split('\\')
+    return name === dirname[dirname.length - 2] || isInArray(name, pickFiles)
+  }
+
+  fs.copy(path.join(__dirname, '../'), path.join(__dirname, '../build'), filter, function(err) {
+    if(err) {
+      throw err
+    }
+    console.log('copy file to build folder finished!'.cyan)
   })
 }
 
